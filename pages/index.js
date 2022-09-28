5e7def4b1d7fd67236fa4346392f32fafc8f6425
meghna-ncompass <meghna@ncompass.inc>
2022-09-28 07:25:27 +0000

    added new api

--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,23 @@ import { GraphQLClient, gql } from "graphql-request";
 
 
 export async function getStaticProps() {
-  const graphcms = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT);
-  const response = await graphcms.request(getArticlesQuery)
-  console.log('RESPONSE',response)
+  // const graphcms = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT);
+  // const response = await graphcms.request(getArticlesQuery)
+  const response = await fetch(process.env.JSON_PLACEHOLDER)
+  const filteredResponse = await response.json()
   return {
-    props: { articles: response.entries },
+    props: { todos : filteredResponse },
   };
 }
-export default function Home({ articles}) {
+export default function Home({ todos }) {
 
   return (
     <>
       <h1>Scroll.in</h1>
       <h2>Changes Are Made</h2>
-      {articles.map(article => (
-        <div key={article.id}>
-         <p>{article.id}</p>
-         <h3>{article.title}</h3>
+      {todos.map(todo => (
+        <div key={todo.id}>
+         <h3>{todo.title}</h3>
         </div>
       ))}
     </>
